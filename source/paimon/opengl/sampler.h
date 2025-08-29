82e3b2c1317766e2670078a42a30f44ef759b227
jyxiong <jingyu.xiong@foxmail.com>
2025-08-29 11:22:16 +0800

    dev: sampler

---
+++ source/paimon/opengl/sampler.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "paimon/opengl/base/object.h"
+
+namespace paimon {
+
+class Sampler : public NamedObject {
+public:
+  Sampler();
+  ~Sampler();
+
+  Sampler(const Sampler &other) = delete;
+  Sampler &operator=(const Sampler &other) = delete;
+
+  Sampler(Sampler &&other) = default;
+
+  bool is_valid() const override;
+
+public:
+  void bind(GLuint unit);
+
+  template <class T> void get(GLenum property, T *value);
+
+  template <class T> T get(GLenum property);
+
+  template <class T> void set(GLenum property, T value);
+};
+
+} // namespace paimon
