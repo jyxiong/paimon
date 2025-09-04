64a2baa4ee4c0139e1189a096304c7a647bc85a1
jyxiong <jingyu.xiong@foxmail.com>
2025-09-04 10:19:24 +0800

    refactor: simplify get functions with concepts

--- source/paimon/opengl/base/get.h
+++ source/paimon/opengl/base/get.h
@@ -1,122 +1,117 @@
 #pragma once
 
+#include <concepts>
+
 #include "glad/gl.h"
+
+#include "glm/gtc/type_ptr.hpp"
 
 // https://registry.khronos.org/OpenGL-Refpages/gl4/html/glGet.xhtml
 
 namespace paimon {
 
-template <class T> void get(GLenum parameter, T *value);
-
-template <> inline void get<GLboolean>(GLenum parameter, GLboolean *value) {
-  glGetBooleanv(parameter, value);
+template <typename T> auto get(GLenum pname) {
+  if constexpr (std::same_as<T, GLboolean>) {
+    GLboolean value;
+    glGetBooleanv(pname, &value);
+    return value;
+  } else if constexpr (std::same_as<T, GLfloat>) {
+    GLfloat value;
+    glGetFloatv(pname, &value);
+    return value;
+  } else if constexpr (std::same_as<T, GLdouble>) {
+    GLdouble value;
+    glGetDoublev(pname, &value);
+    return value;
+  } else if constexpr (std::same_as<T, GLint>) {
+    GLint value;
+    glGetIntegerv(pname, &value);
+    return value;
+  } else if constexpr (std::same_as<T, GLint64>) {
+    GLint64 value;
+    glGetInteger64v(pname, &value);
+    return value;
+  } else {
+    static_assert(std::false_type::value, "Unsupported type for glGet");
+  }
 }
 
-template <> inline void get<GLfloat>(GLenum parameter, GLfloat *value) {
-  glGetFloatv(parameter, value);
+template <typename T> auto get(GLenum pname, GLuint index) {
+  if constexpr (std::same_as<T, GLboolean>) {
+    GLboolean value;
+    glGetBooleani_v(pname, index, &value);
+    return value;
+  } else if constexpr (std::same_as<T, GLfloat>) {
+    GLfloat value;
+    glGetFloati_v(pname, index, &value);
+    return value;
+  } else if constexpr (std::same_as<T, GLdouble>) {
+    GLdouble value;
+    glGetDoublei_v(pname, index, &value);
+    return value;
+  } else if constexpr (std::same_as<T, GLint>) {
+    GLint value;
+    glGetIntegeri_v(pname, index, &value);
+    return value;
+  } else if constexpr (std::same_as<T, GLint64>) {
+    GLint64 value;
+    glGetInteger64i_v(pname, index, &value);
+    return value;
+  } else {
+    static_assert(std::false_type::value, "Unsupported type for glGet");
+  }
 }
 
-template <> inline void get<GLdouble>(GLenum parameter, GLdouble *value) {
-  glGetDoublev(parameter, value);
+template <int L, typename T> auto gets(GLenum pname) {
+  if constexpr (std::same_as<T, GLboolean>) {
+    glm::vec<L, GLboolean> value;
+    glGetBooleanv(pname, glm::value_ptr(value));
+    return value;
+  } else if constexpr (std::same_as<T, GLfloat>) {
+    glm::vec<L, GLfloat> value;
+    glGetFloatv(pname, glm::value_ptr(value));
+    return value;
+  } else if constexpr (std::same_as<T, GLdouble>) {
+    glm::vec<L, GLdouble> value;
+    glGetDoublev(pname, glm::value_ptr(value));
+    return value;
+  } else if constexpr (std::same_as<T, GLint>) {
+    glm::vec<L, GLint> value;
+    glGetIntegerv(pname, glm::value_ptr(value));
+    return value;
+  } else if constexpr (std::same_as<T, GLint64>) {
+    glm::vec<L, GLint64> value;
+    glGetInteger64v(pname, glm::value_ptr(value));
+    return value;
+  } else {
+    static_assert(std::false_type::value, "Unsupported type for glGet");
+  }
 }
 
-template <> inline void get<GLint>(GLenum parameter, GLint *value) {
-  glGetIntegerv(parameter, value);
-}
-
-template <> inline void get<GLint64>(GLenum parameter, GLint64 *value) {
-  glGetInteger64v(parameter, value);
-}
-
-template <class T> T get(GLenum parameter);
-
-template <> inline GLboolean get<GLboolean>(GLenum parameter) {
-  GLboolean value;
-  get(parameter, &value);
-  return value;
-}
-
-template <> inline GLfloat get<GLfloat>(GLenum parameter) {
-  GLfloat value;
-  get(parameter, &value);
-  return value;
-}
-
-template <> inline GLdouble get<GLdouble>(GLenum parameter) {
-  GLdouble value;
-  get(parameter, &value);
-  return value;
-}
-
-template <> inline GLint get<GLint>(GLenum parameter) {
-  GLint value;
-  get(parameter, &value);
-  return value;
-}
-
-template <> inline GLint64 get<GLint64>(GLenum parameter) {
-  GLint64 value;
-  get(parameter, &value);
-  return value;
-}
-
-template <class T> void get(GLenum parameter, GLuint index, T *value);
-
-template <>
-inline void get<GLboolean>(GLenum parameter, GLuint index, GLboolean *value) {
-  glGetBooleani_v(parameter, index, value);
-}
-
-template <>
-inline void get<GLfloat>(GLenum parameter, GLuint index, GLfloat *value) {
-  glGetFloati_v(parameter, index, value);
-}
-
-template <>
-inline void get<GLdouble>(GLenum parameter, GLuint index, GLdouble *value) {
-  glGetDoublei_v(parameter, index, value);
-}
-
-template <>
-inline void get<GLint>(GLenum parameter, GLuint index, GLint *value) {
-  glGetIntegeri_v(parameter, index, value);
-}
-
-template <>
-inline void get<GLint64>(GLenum parameter, GLuint index, GLint64 *value) {
-  glGetInteger64i_v(parameter, index, value);
-}
-
-template <class T> T get(GLenum parameter, GLuint index);
-
-template <> inline GLboolean get<GLboolean>(GLenum parameter, GLuint index) {
-  GLboolean value;
-  get(parameter, index, &value);
-  return value;
-}
-
-template <> inline GLfloat get<GLfloat>(GLenum parameter, GLuint index) {
-  GLfloat value;
-  get(parameter, index, &value);
-  return value;
-}
-
-template <> inline GLdouble get<GLdouble>(GLenum parameter, GLuint index) {
-  GLdouble value;
-  get(parameter, index, &value);
-  return value;
-}
-
-template <> inline GLint get<GLint>(GLenum parameter, GLuint index) {
-  GLint value;
-  get(parameter, index, &value);
-  return value;
-}
-
-template <> inline GLint64 get<GLint64>(GLenum parameter, GLuint index) {
-  GLint64 value;
-  get(parameter, index, &value);
-  return value;
+template <int L, typename T> auto gets(GLenum pname, GLuint index) {
+  if constexpr (std::same_as<T, GLboolean>) {
+    glm::vec<L, GLboolean> value;
+    glGetBooleani_v(pname, index, glm::value_ptr(value));
+    return value;
+  } else if constexpr (std::same_as<T, GLfloat>) {
+    glm::vec<L, GLfloat> value;
+    glGetFloati_v(pname, index, glm::value_ptr(value));
+    return value;
+  } else if constexpr (std::same_as<T, GLdouble>) {
+    glm::vec<L, GLdouble> value;
+    glGetDoublei_v(pname, index, glm::value_ptr(value));
+    return value;
+  } else if constexpr (std::same_as<T, GLint>) {
+    glm::vec<L, GLint> value;
+    glGetIntegeri_v(pname, index, glm::value_ptr(value));
+    return value;
+  } else if constexpr (std::same_as<T, GLint64>) {
+    glm::vec<L, GLint64> value;
+    glGetInteger64i_v(pname, index, glm::value_ptr(value));
+    return value;
+  } else {
+    static_assert(std::false_type::value, "Unsupported type for glGet");
+  }
 }
 
 } // namespace paimon
