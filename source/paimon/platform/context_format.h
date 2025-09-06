221f270116ad2494b3f1cb41eeb1d92e9fdbc8f5
jyxiong <jingyu.xiong@foxmail.com>
2025-09-06 12:49:55 +0800

    feat: impl glx context

--- source/paimon/platform/context_format.h
+++ source/paimon/platform/context_format.h
@@ -5,7 +5,6 @@ namespace paimon {
 enum class ContextProfile : unsigned int { NONE, Core, Compatibility };
 
 struct ContextFormat {
-  enum class Profile { Core, Compatibility, None };
   unsigned int versionMajor = 0;
   unsigned int versionMinor = 0;
   ContextProfile profile = ContextProfile::NONE;
