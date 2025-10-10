413e650f9a44de84bf4a277a0eb540d23283571e
jyxiong <jingyu.xiong@foxmail.com>
2025-10-10 11:21:50 +0800

    feat: impl glfw context (#24)

---
+++ source/paimon/platform/glfw/context.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "paimon/platform/context.h"
+#include "paimon/platform/context_format.h"
+
+#include "GLFW/glfw3.h"
+
+namespace paimon {
+
+class ContextImpl : public Context {
+public:
+  ContextImpl();
+  ~ContextImpl();
+
+  bool destroy() override;
+
+  long long nativeHandle() const override;
+
+  bool valid() const override;
+
+  bool makeCurrent() const override;
+
+  bool doneCurrent() const override;
+
+  static std::unique_ptr<Context> getCurrent();
+
+  static std::unique_ptr<Context> create(const Context& shared, const ContextFormat &format);
+
+  static std::unique_ptr<Context> create(const ContextFormat &format);
+
+private:
+  void createContext(GLFWwindow* shared, const ContextFormat &format);
+
+private:
+  GLFWwindow* m_context = nullptr;
+  bool m_owning;
+};
+
+} // namespace paimon
